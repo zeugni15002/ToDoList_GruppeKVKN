84aab9da3541ddeebc1884a22b51db995ab167fe
kleailjazi <kleailjazi2011@gmail.com>
2025-05-17 15:36:37 +0200

    feature-Setzen
    es wurde ein feature hinzugefügt

--- ToDoList/ToDoListApplication/ToDoListApplication/Program.cs
+++ ToDoList/ToDoListApplication/ToDoListApplication/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Ich mache die Formatierung");
+
